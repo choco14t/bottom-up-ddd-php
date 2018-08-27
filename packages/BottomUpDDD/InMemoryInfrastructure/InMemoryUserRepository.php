5c076e8a2c87abc3342c8326d7fb0255b2a1d72c
chocol4t <y.masuda1402+git97hub@gmail.com>
2018-08-28 07:15:32 +0900

    feat: implement test repository

--- packages/BottomUpDDD/InMemoryInfrastructure/InMemoryUserRepository.php
+++ packages/BottomUpDDD/InMemoryInfrastructure/InMemoryUserRepository.php
@@ -9,23 +9,43 @@ use BottomUpDDD\Domain\Users\UserName;
 
 final class InMemoryUserRepository implements UserRepositoryInterface
 {
+    /** @var User[] */
+    private $data = [];
+
     public function findByUserId(UserId $userId)
     {
+        $id = $userId->value();
+
+        if (array_key_exists($id, $data)) {
+            return $data[$id];
+        }
+
+        return null;
     }
 
     public function findByUserName(UserName $userName)
     {
+        foreach ($this->data as $user) {
+            if ($user->userName()->equals($userName)) {
+                return $user;
+            }
+        }
+
+        return null;
     }
 
     public function findAll()
     {
+        return array_values($data);
     }
 
     public function save(User $user)
     {
+        $this->data[$user->id()->value()] = $user;
     }
 
-    public function delete(User $user)
+    public function delete(UserId $userId)
     {
+        unset($this->data[$userId->value()]);
     }
 }
