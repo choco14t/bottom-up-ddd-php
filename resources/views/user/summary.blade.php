18ab7edfb5b549d5259a298eadc757c552ad694f
chocol4t <y.masuda1402+git97hub@gmail.com>
2018-09-23 23:42:02 +0900

    add: summary.blade

---
+++ resources/views/user/summary.blade.php
@@ -0,0 +1,36 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Users</div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="text-center">UserName</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($summary as $user)
+                                <tr>
+                                    <td class="text-center">{{ $user->userName }}</td>
+                                    <td class="text-center">
+                                        <a href="{{ route('detail', ['id' => $user->id]) }}" class="btn btn-info">Detail</a>
+                                        <a class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
